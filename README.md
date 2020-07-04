3c0c0f9a38f7a06c979df20c37a76a3ab854cadb
YJjason <1009239228@qq.com>
2020-07-04 13:01:53 +0800

    商品列表页面二级筛选导航布局

--- README.md
+++ README.md
@@ -19,14 +19,17 @@ samples, guidance on mobile development, and a full API reference.
  封装适配库以及实现左右滑动ListView
  1.使用 flutter_screenutil 组件适配不同手机尺寸
  2.实现左右滑动ListView
- ## chapter_03
+## chapter_03
   首页商品列表布局
- ## chapter_04
+## chapter_04
   JSON和序列化反序列化、创建模型类转换Json数据、轮播图数据渲染:
   Flutter创建商品数据模型 、请求Api接口渲染热门商品 推荐商品
- ## chapter_05
+## chapter_05
   商品分类数据模型CateModel.dart 页面数据请求展示
-  ## chapter_06
+## chapter_06
   AutomaticKeepAliveClientMixin：保此部分页面的状态：
-   ## chapter_06
-   商品分类跳转到商品列表传值 商品列表页面布局
+## chapter_06
+  商品分类跳转到商品列表传值 商品列表页面布局
+## chapter_07
+  商品列表页面二级筛选导航布局
+
--- lib/pages/ProductList.dart
+++ lib/pages/ProductList.dart
@@ -5,90 +5,180 @@ import './../config/config.dart';
 import 'package:dio/dio.dart';
 
 class ProductListPage extends StatefulWidget {
-  ProductListPage({Key key, arguments}) : super(key: key);
+  Map arguments;
+  ProductListPage({Key key, this.arguments}) : super(key: key);
 
   @override
   _ProductListPageState createState() => _ProductListPageState();
 }
 
 class _ProductListPageState extends State<ProductListPage> {
+//通过事件打开侧边栏，需要全局声明一下：
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+
+  //筛选导航：
+  Widget _subHeaderWidget() {
+    return Positioned(
+      top: 0,
+      height: ScreenUtil().setHeight(80),
+      width: ScreenUtil().setWidth(750),
+      child: Container(
+        height: ScreenUtil().setHeight(80),
+        width: ScreenUtil().setWidth(750),
+        decoration: BoxDecoration(
+          border: Border(
+            bottom: BorderSide(
+              width: 1,
+              color: Color.fromRGBO(233, 233, 233, 0.9),
+            ),
+          ),
+        ),
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              flex: 1,
+              child: InkWell(
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(0, ScreenUtil().setHeight(20), 0,
+                      ScreenUtil().setHeight(20)),
+                  child: Text(
+                    '综合',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.red),
+                  ),
+                ),
+                onTap: () {},
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: InkWell(
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(0, ScreenUtil().setHeight(20), 0,
+                      ScreenUtil().setHeight(20)),
+                  child: Text('销量', textAlign: TextAlign.center),
+                ),
+                onTap: () {},
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: InkWell(
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(0, ScreenUtil().setHeight(20), 0,
+                      ScreenUtil().setHeight(20)),
+                  child: Text('价格', textAlign: TextAlign.center),
+                ),
+                onTap: () {},
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: InkWell(
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(0, ScreenUtil().setHeight(20), 0,
+                      ScreenUtil().setHeight(20)),
+                  child: Text('筛选', textAlign: TextAlign.center),
+                ),
+                onTap: () {
+                  _scaffoldKey.currentState.openEndDrawer();
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // 商品列表
+  Widget _productListWidget() {
+    return Container(
+      padding: EdgeInsets.all(10),
+      margin: EdgeInsets.only(top: ScreenUtil().setHeight(80)),
+      child: ListView.builder(
+        // itemCount: 2,
+        itemBuilder: (context, index) {
+          //获得每一个元素：
+          return Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Container(
+                    width: ScreenUtil().setWidth(180),
+                    height: ScreenUtil().setHeight(180),
+                    child: Image.network(
+                      "https://www.itying.com/images/flutter/list2.jpg",
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      height: ScreenUtil().setHeight(180),
+                      margin: EdgeInsets.only(left: 10),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            'erwrwerwe',
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          Row(
+                            children: <Widget>[
+                              Container(
+                                height: ScreenUtil().setHeight(36),
+                                margin: EdgeInsets.only(right: 10),
+                                padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                                //注意：如果Container里面加上decoration属性，这个时候color属性必须放到BoxDecoration
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                child: Text('4g'),
+                              ),
+                              Container(
+                                height: ScreenUtil().setHeight(36),
+                                margin: EdgeInsets.only(right: 10),
+                                padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                                //注意：如果Container里面加上decoration属性，这个时候color属性必须放到BoxDecoration
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  // color:Color.fromRGBO(230, 230, 230, 0.3)
+                                ),
+                                child: Text('16G'),
+                              ),
+                            ],
+                          ),
+                          Text('￥2999', style: TextStyle(color: Colors.red))
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              Divider(
+                height: 20,
+              )
+            ],
+          );
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('商品列表'),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(10),
-        child: ListView.builder(
-          // itemCount: 2,
-          itemBuilder: (context, index) {
-            //获得每一个元素：
-            return Column(
-              children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    Container(
-                      width: ScreenUtil().setWidth(180),
-                      height: ScreenUtil().setHeight(180),
-                      child: Image.network(
-                        "https://www.itying.com/images/flutter/list2.jpg",
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: Container(
-                        height: ScreenUtil().setHeight(180),
-                        margin: EdgeInsets.only(left: 10),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(
-                              'erwrwerwe',
-                              maxLines: 2,
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                            Row(
-                              children: <Widget>[
-                                Container(
-                                  height: ScreenUtil().setHeight(36),
-                                  margin: EdgeInsets.only(right: 10),
-                                  padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                                  //注意：如果Container里面加上decoration属性，这个时候color属性必须放到BoxDecoration
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(10),
-                                  ),
-                                  child: Text('4g'),
-                                ),
-                                Container(
-                                  height: ScreenUtil().setHeight(36),
-                                  margin: EdgeInsets.only(right: 10),
-                                  padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                                  //注意：如果Container里面加上decoration属性，这个时候color属性必须放到BoxDecoration
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(10),
-                                    // color:Color.fromRGBO(230, 230, 230, 0.3)
-                                  ),
-                                  child: Text('16G'),
-                                ),
-                              ],
-                            ),
-                            Text('￥2999', style: TextStyle(color: Colors.red))
-                          ],
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-                Divider(
-                  height: 20,
-                )
-              ],
-            );
-          },
-        ),
+      body: Stack(
+        children: <Widget>[
+          _subHeaderWidget(),
+          _productListWidget(),
+        ],
       ),
     );
   }
